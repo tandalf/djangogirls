46ad76bfd5a404865a97b2f6f841ff71792e82dd
Timothy Ebiuwhe <timothy_ebiuwhe@live.com>
2018-09-17 01:53:59 +0100

    Added post details page

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
 from datetime import datetime
 
 from .models import Post
@@ -6,3 +8,8 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=datetime.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+    
